3d9f7a9258d0eedf170e09abc55a249aab9a3336
jdbben <benrahmounjamaleddine@gmail.com>
2026-01-19 14:18:16 +0100

    giving a randome w h to dimensions

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -35,7 +35,10 @@ const UserImage: React.FC<Props> = ({ session, lastImg }) => {
     fetchDimensions();
   }, [lastImg]);
 
-  if (!lastImg || !dimensions) return <div>Loading...</div>
+  if (!lastImg || !dimensions){ 
+    setDimensions({ width: 10, height: 10 }) 
+    return <div>Loading...</div>
+  }
 
   return <ResizeImg img={lastImg} dimensions={dimensions} userName={session?.user?.name} />
 };
